1e8ffdc70d6994b8ce4842af61cffd6c18ea5b9e
Peter Olah <rehabit.munka@gmail.com>
2024-06-10 14:12:02 +0200

    spring-email-poc: implemented html email sending as well

--- src/main/java/com/peterolah/example/email/controller/EmailController.java
+++ src/main/java/com/peterolah/example/email/controller/EmailController.java
@@ -1,9 +1,12 @@
 package com.peterolah.example.email.controller;
 
 import com.peterolah.example.email.service.EmailService;
+import jakarta.mail.MessagingException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/email")
@@ -17,5 +20,14 @@ public class EmailController {
     @RequestMapping(value = "send-simple-text", method = RequestMethod.GET)
     public void sendSimpleText() {
         emailService.sendSimpleText();
+    }
+
+    @RequestMapping(value = "send-html", method = RequestMethod.GET)
+    public void sendHtml() {
+        try {
+            emailService.sendHtml();
+        } catch (MessagingException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/peterolah/example/email/service/EmailService.java
+++ src/main/java/com/peterolah/example/email/service/EmailService.java
@@ -1,32 +1,63 @@
 package com.peterolah.example.email.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
     public static final String SUBJECT_AND_BODY = "árvíztűrő tükörfúrógép - ÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP";
-    private final MailSender emailSender;
+    public static final String FROM_AND_TO = "rehabit.munka@gmail.com";
+    private final MailSender mailSender;
 
-    public EmailService(MailSender emailSender) {
-        this.emailSender = emailSender;
+    private final JavaMailSender javaMailSender;
+
+    @Value("classpath:email-templates/mail-logo.png")
+    private Resource resourceFile;
+
+
+    public EmailService(MailSender mailSender, JavaMailSender javaMailSender) {
+        this.mailSender = mailSender;
+        this.javaMailSender = javaMailSender;
     }
 
     public void sendSimpleText() {
 
         final var message = new SimpleMailMessage();
-            message.setFrom("rehabit.munka@gmail.com");
-            message.setTo("rehabit.munka@gmail.com");
+            message.setFrom(FROM_AND_TO);
+            message.setTo(FROM_AND_TO);
             message.setSubject(SUBJECT_AND_BODY);
             message.setText(SUBJECT_AND_BODY);
 
         try {
-            emailSender.send(message);
+            mailSender.send(message);
         } catch (MailException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void sendHtml () throws MessagingException, IOException {
+        String htmlBody = new String(new ClassPathResource("/email-templates/template.html").getContentAsByteArray(), StandardCharsets.UTF_8 );
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setFrom(FROM_AND_TO);
+        helper.setTo(FROM_AND_TO);
+        helper.setSubject("[html]" + SUBJECT_AND_BODY);
+
+        helper.setText(htmlBody, true);
+        helper.addInline("attachment.png", resourceFile);
+        javaMailSender.send(message);
     }
 
 
